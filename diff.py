5e1c839a62a5947bff6e17b3674b5b25c3aef220
varshini-07 <78426186+varshini-07@users.noreply.github.com>
2021-08-27 13:54:46 +0530

    update_aug27
    Add of n odd numbers

--- diff.py
+++ diff.py
@@ -1,10 +1,15 @@
 n=int(input("enter the number:"))               #n=10
 result=0
-for i in range(1,n):                       # repeated inputs
+for i in range(1,n,2):                       # repeated inputs
  # different_inputs=int(input("enter the {} different numbers".format(n))) 
-    different_inputs=int(input(f"Enter the value for  {i}:  "))
-    result=result+different_inputs
+    #different_inputs=int(input(f"Enter the value for  {i}:  "))
+    print("The adding value ",i)
+    result=result+i
 print("The result is  ",result)
 
 #>enter the number:10
 #enter the 10 different numbers: 
+
+
+#range(start,stop,step)
+#range(1,n,2)
