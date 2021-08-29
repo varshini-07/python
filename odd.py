e9a992b96cc54ee6d48cb3e661ab5f4c09fdedbf
varshini-07 <78426186+varshini-07@users.noreply.github.com>
2021-08-29 12:10:25 +0530

    Add files via upload

---
+++ odd.py
@@ -0,0 +1,8 @@
+n=int(input("enter the number:"))               #n=10
+result=0
+for i in range(1,n,2):                       # repeated inputs
+ # different_inputs=int(input("enter the {} different numbers".format(n))) 
+    #different_inputs=int(input(f"Enter the value for  {i}:  "))
+    print("The adding value ",i)
+    result=result+i
+print("The result is  ",result)
