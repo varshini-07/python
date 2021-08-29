a92f33743399ed1cccd0a27b9c9acb1d01776156
varshini-07 <78426186+varshini-07@users.noreply.github.com>
2021-08-29 12:06:46 +0530

    Delete n_nos.py

--- n_nos.py
+++
@@ -1,6 +0,0 @@
-n=int(input("enter the number:"))               
-result=0
-for i in range(1,n):                       # repeated inputs
-    different_inputs=int(input(f"Enter the value for  {i}:  "))
-    result=result+different_inputs
-print("The result is  ",result)
