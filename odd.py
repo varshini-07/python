1c71c80bcfd4013c5fc4bba39dbc05ed3bf404da
varshini-07 <78426186+varshini-07@users.noreply.github.com>
2021-08-29 12:18:16 +0530

    initial commit

--- odd.py
+++ odd.py
@@ -1,6 +1,6 @@
 n=int(input("enter the number:"))               #n=10
 result=0
 for i in range(1,n,2):                       # repeated inputs
-     print("The adding value ",i)
+    print("The adding value ",i)
     result=result+i
 print("The result is  ",result)
